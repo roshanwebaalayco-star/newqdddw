138a55df03bcf8f399c135e7e180d9c848bc5281
roshanwebaalayco-star <roshan%webaalay.co@gtempaccount.com>
2025-10-19 13:39:58 +0530

    Replace logo component with branded artwork

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -257,7 +257,7 @@ export default function Home() {
     data: {
       name: "CLC Retail Group",
       url: `${baseUrl}/`,
-      logo: `${baseUrl}/logo.png`,
+      logo: `${baseUrl}/logo.jpeg`,
       contactPoint: [
         {
           "@type": "ContactPoint",
